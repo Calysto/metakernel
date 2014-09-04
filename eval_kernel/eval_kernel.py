86cf16611fe4126f4345477b24da5c15fed4c1e8
Steven Silvester <steven.silvester@ieee.org>
2014-09-03 21:37:52 -0500

    Update eval kernel to use python magic.

--- eval_kernel/eval_kernel.py
+++ eval_kernel/eval_kernel.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 from jupyter_kernel import MagicKernel
-import os
+
 
 class EvalKernel(MagicKernel):
     implementation = 'Eval'
@@ -20,17 +20,25 @@ class EvalKernel(MagicKernel):
         """
         self.env[name] = value
 
-    def get_kernel_help_on(self, expr, level):
-        return "Sorry, no help is available on '%s'." % expr
+    def get_variable(self, name):
+        """
+        Get a variable from the kernel language.
+        """
+        return self.env.get(name, None)
 
     def do_execute_direct(self, code):
-        try:
-            return eval(code.strip(), self.env)
-        except:
-            try:
-                exec code.strip() in self.env
-            except:
-                return "Error: " + code
+        python_magic = self.line_magics['python']
+        resp = python_magic.eval(code.strip())
+        if not resp is None:
+            self.Print(str(resp))
+
+    def get_completions(self, token):
+        python_magic = self.line_magics['python']
+        return python_magic.get_completions(token)
+
+    def get_kernel_help_on(self, expr, level=0):
+        python_magic = self.line_magics['python']
+        return python_magic.get_help_on(expr, level)
 
 if __name__ == '__main__':
     from IPython.kernel.zmq.kernelapp import IPKernelApp
