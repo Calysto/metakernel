a0c84ea94bee0c9814db24d0fdfe6f444ee6b6c0
Douglas Blank <doug.blank@gmail.com>
2014-09-13 07:39:40 -0400

    eval kernel: fix for ability to use %pmap; fixes do_function_direct

--- eval_kernel/eval_kernel.py
+++ eval_kernel/eval_kernel.py
@@ -36,6 +36,7 @@ class EvalKernel(MagicKernel):
         """
         Call a function in the kernel language with args (as a single item).
         """
+        python_magic = self.line_magics['python']
         return python_magic.eval("%s(%s)" % (function_name, arg))
 
     def get_completions(self, info):
