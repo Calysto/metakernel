54ee6f73ede2ade156116e305dfdbc543e119809
Doug Blank <dblank@cs.brynmawr.edu>
2014-11-08 12:04:31 -0500

    Added generate_help.py to be run by ipython console --kernel eval_kernel

---
+++ generate_help.py
@@ -0,0 +1,14 @@
+
+text = "# Line Magics\n\n"
+for magic in sorted(kernel.line_magics.keys()):
+    text += "## `%" + magic + "`\n\n"
+    text += kernel.get_help_on("%" + magic) + "\n\n"
+
+text = "# Cell Magics\n\n"
+for magic in sorted(kernel.cell_magics.keys()):
+    text += "## `%%" + magic + "`\n\n"
+    text += kernel.get_help_on("%%" + magic) + "\n\n"
+
+fp = open("MagicHelp.md", "w")
+fp.write(text)
+fp.close()
