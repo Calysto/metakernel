a94e2f5498482cff2059f5ebf0b81c225616c0d0
Doug Blank <dblank@cs.brynmawr.edu>
2014-11-09 18:24:45 -0500

    Added tests for reload_magics

---
+++ metakernel/magics/tests/test_reload_magics_magic.py
@@ -0,0 +1,11 @@
+
+from metakernel.tests.utils import get_kernel, get_log_text
+
+def test_reload_magics_magic():
+    kernel = get_kernel()
+    kernel.do_execute("%reload_magics")
+    text = get_log_text(kernel)
+
+    for item in "%cd %connect_info %download %edit %help %html %install_magic %javascript %kernel %kx %latex %load %lsmagic %magic %parallel %plot %pmap %px %python %reload_magics %restart %run %shell %spell %%debug %%file %%help %%html %%javascript %%kx %%latex %%processing %%px %%python %%shell %%show %%spell %%time".split():
+        assert item in text, ("load_magic didn't list '%s'" % item)
+
