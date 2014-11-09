92bd8eaa26b7b47854a7d906e8e42267659ec116
Doug Blank <dblank@cs.brynmawr.edu>
2014-11-08 22:59:04 -0500

    added test_spell_magic

--- metakernel/magics/tests/test_spell_magic.py
+++ metakernel/magics/tests/test_spell_magic.py
@@ -1,7 +1,27 @@
 
-from metakernel.tests.utils import get_kernel, get_log_text
+from metakernel.tests.utils import (get_kernel, get_log_text, EvalKernel,
+                                    clear_log_text)
 
 
 def test_spell_magic():
-    kernel = get_kernel()
+    kernel = get_kernel(EvalKernel)
+    kernel.do_execute("""%%spell testme
+print "ok"
+    """, False)
+    kernel.do_execute("%spell testme", False)
+    text = get_log_text(kernel)
+    assert "ok" in text, text
+    clear_log_text(kernel)
+
+    kernel.do_execute("%spell -l learned", False)
+    text = get_log_text(kernel)
+    assert "testme" in text, text
+    clear_log_text(kernel)
+
+    kernel.do_execute("%spell -d testme", False)
+    kernel.do_execute("%spell -l learned", False)
+    text = get_log_text(kernel)
+    assert "testme" not in text, text
+    clear_log_text(kernel)
+
     
