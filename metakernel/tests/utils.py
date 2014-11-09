6282c4627fb889b0dc188147221f5f0c67ceefda
Steven Silvester <steven.silvester@ieee.org>
2014-11-08 21:55:10 -0600

    Remove unused portion of the eval kernel (fill in as needed)

--- metakernel/tests/utils.py
+++ metakernel/tests/utils.py
@@ -15,46 +15,12 @@ class EvalKernel(MetaKernel):
     language_version = '0.1'
     banner = "Eval kernel - evaluates simple Python statements and expressions"
 
-    def get_usage(self):
-        return ("This is the Eval kernel. It implements a simple Python " +
-                "interpreter.")
-
     def set_variable(self, name, value):
         """
         Set a variable in the kernel language.
         """
         python_magic = self.line_magics['python']
         python_magic.env[name] = value
-
-    def get_variable(self, name):
-        """
-        Get a variable from the kernel language.
-        """
-        python_magic = self.line_magics['python']
-        return python_magic.env.get(name, None)
-
-    def do_execute_direct(self, code):
-        python_magic = self.line_magics['python']
-        return python_magic.eval(code.strip())
-
-    def do_execute_file(self, filename):
-        code = "".join(open(filename).readlines())
-        return self.do_execute_direct(code)
-
-    def do_function_direct(self, function_name, arg):
-        """
-        Call a function in the kernel language with args (as a single item).
-        """
-        python_magic = self.line_magics['python']
-        return python_magic.eval("%s(%s)" % (function_name, arg))
-
-    def get_completions(self, info):
-        python_magic = self.line_magics['python']
-        return python_magic.get_completions(info)
-
-    def get_kernel_help_on(self, info, level=0, none_on_fail=False):
-        python_magic = self.line_magics['python']
-        return python_magic.get_help_on(info, level, none_on_fail)
 
 
 def get_kernel(kernel_class=MetaKernel):
