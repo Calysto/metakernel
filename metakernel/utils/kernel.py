66d159fb1800b5e55c057316ac53fb62f1eb6b6e
Doug Blank <dblank@cs.brynmawr.edu>
2014-12-09 23:00:42 -0500

    Write bytes; make Travis happy

--- metakernel/utils/kernel.py
+++ metakernel/utils/kernel.py
@@ -7,5 +7,5 @@ def install_kernel_resources(destination):
     """
     for filename in ["logo-64x64.png", "logo-32x32.png"]:
         data = pkgutil.get_data("metakernel", filename)
-        with open(os.path.join(destination, filename), "w") as fp:
+        with open(os.path.join(destination, filename), "wb") as fp:
             fp.write(data)
