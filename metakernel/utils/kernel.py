777d8b87bca9432bef0698b7557ece9740cef482
Doug Blank <dblank@cs.brynmawr.edu>
2014-12-13 09:29:15 -0500

    Allow installing resources

--- metakernel/utils/kernel.py
+++ metakernel/utils/kernel.py
@@ -1,11 +1,13 @@
 import pkgutil
 import os
 
-def install_kernel_resources(destination):
+def install_kernel_resources(destination, resource="metakernel", files=None):
     """
     Copy the resource files to the kernelspec folder.
     """
-    for filename in ["logo-64x64.png", "logo-32x32.png"]:
-        data = pkgutil.get_data("metakernel", filename)
+    if files is None:
+        files = ["logo-64x64.png", "logo-32x32.png"]
+    for filename in files:
+        data = pkgutil.get_data(resource, filename)
         with open(os.path.join(destination, filename), "wb") as fp:
             fp.write(data)
