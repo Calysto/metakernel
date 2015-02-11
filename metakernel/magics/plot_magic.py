fa55f976f4a41bfcbf2009e543fd2a0057451c31
Steven Silvester <steven.silvester@ieee.org>
2015-02-10 21:01:30 -0600

    Fix the option and update the note

--- metakernel/magics/plot_magic.py
+++ metakernel/magics/plot_magic.py
@@ -18,7 +18,10 @@ class PlotMagic(Magic):
         '-b', '--backend', action='store', default='inline',
         help='Backend selection'
     )
-    def line_plot(self, backend=None, size=None, format=None):
+    @option(
+        '-r', '--resolution', action='store', default=96,
+        help='Resolution in pixels per inch'
+    )
     def line_plot(self, backend=None, size=None, format=None, resolution=96):
         """
         %plot [options] backend - configure plotting for the session.
@@ -30,7 +33,8 @@ class PlotMagic(Magic):
             %plot --backend=qt --format=png
             %plot -b inline -s 640,480
 
-        Note: not all languages may support the %plot magic.
+        Note: not all languages may support the %plot magic, and not all
+        options may be supported.
         """
         self.kernel.update_plot_settings(backend, size, format, resolution)
         self.kernel.handle_plot_settings()
