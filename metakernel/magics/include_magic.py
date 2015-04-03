d8546c36980a45e9676f3a9a6b1d306597d5560e
Doug Blank <dblank@cs.brynmawr.edu>
2015-04-02 22:57:31 -0400

    Refined %include magics to allow multiple includes both as stacked %includes, and multiple includes on a line

--- metakernel/magics/include_magic.py
+++ metakernel/magics/include_magic.py
@@ -6,9 +6,9 @@ import os
 
 class IncludeMagic(Magic):
 
-    def line_include(self, filename):
+    def line_include(self, filenames):
         """
-        %include FILENAME - include code from filename into this code
+        %include FILENAME ... - include code from filename into this code
 
         This line magic will get the contents of a file and include it
         in this cell evaluation.
@@ -16,13 +16,29 @@ class IncludeMagic(Magic):
         Example:
             %include myprog.py
         """
-        if filename.startswith("~"):
-            filename = os.path.expanduser(filename)
-        filename = os.path.abspath(filename)
-        text = open(filename).read()
-        self.code = text  + self.code
+        text = ""
+        filenames = filenames.split()
+        for filename in filenames:
+            if filename.startswith("~"):
+                filename = os.path.expanduser(filename)
+            filename = os.path.abspath(filename)
+            text += open(filename).read()
+        if self.code.startswith("%"):
+            lines = self.code.split("\n")
+            new_lines = []
+            need_to_insert = True
+            for line in lines:
+                if need_to_insert and not line.startswith("%"):
+                    new_lines.append(text)
+                    need_to_insert = False
+                new_lines.append(line)
+            if need_to_insert:
+                new_lines.append(text)
+            self.code = "\n".join(new_lines)
+        else:
+            self.code = text  + self.code
 
-    def cell_include(self, filename):
+    def cell_include(self, filename, *args):
         """
         %%include FILENAME - include code from filename into this code
 
--- metakernel/magics/tests/test_include_magic.py
+++ metakernel/magics/tests/test_include_magic.py
@@ -10,11 +10,19 @@ def test_include_magic():
         global EXECUTION
         EXECUTION = code
     kernel.do_execute_direct = do_execute_direct
-    kernel.do_execute("%%include %s" % __file__)
+    FILE = __file__
+    if FILE.endswith(".pyc"):
+        FILE = FILE[:-1]
+    kernel.do_execute("%%include %s" % FILE)
     assert "metakernel" in EXECUTION
-    assert "AND " + "THIS" not in EXECUTION
+    assert ("AND " + "THIS") not in EXECUTION
 
     EXECUTION = ""
-    kernel.do_execute(("%%%%include %s\nAND" + " THIS") % __file__)
+    kernel.do_execute(("%%%%include %s\nAND" + " THIS") % FILE)
     assert "metakernel" in EXECUTION
-    assert "AND " + "THIS" in EXECUTION
+    assert ("AND " + "THIS") in EXECUTION
+
+    EXECUTION = ""
+    kernel.do_execute("%%include '%s' '%s'" % (FILE, FILE))
+    assert "metakernel" in EXECUTION
+    assert ("AND " + "THIS") not in EXECUTION
