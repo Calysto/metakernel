ef235e2587d68ef8f6d1b78673d1721ef3a7090f
Doug Blank <dblank@cs.brynmawr.edu>
2015-09-18 06:51:30 -0400

    Jigsaw magic takes --workspace flag for named workspace (works between invocations)

--- metakernel/magics/jigsaw_magic.py
+++ metakernel/magics/jigsaw_magic.py
@@ -3,6 +3,8 @@
 
 from metakernel import Magic, option
 from IPython.display import HTML
+import string
+import random
 import urllib
 try:
     import urlparse
@@ -24,21 +26,30 @@ except: # python3
 
 class JigsawMagic(Magic):
 
-    def line_jigsaw(self, language):
+    @option(
+        '-w', '--workspace', action='store', default=None,
+        help='use the provided name as workspace filename'
+    )
+    def line_jigsaw(self, language, workspace=None):
         """
         %jigsaw LANGUAGE - show visual code editor/generator
 
         This line magic will allow visual code editing or generation.
 
-        Example:
+        Examples:
             %jigsaw Processing
+            %jigsaw Python
+            %jigsaw Processing --workspace prog1.xml
         """
         # Copy iframe html to here (must come from same domain):
         ##if not os.path.isfile("Processing.html"):
         download("https://calysto.github.io/jigsaw/" + language + ".html", 
                  language + ".html")
+        # Make up a random workspace name:
+        if workspace is None:
+            workspace = ".jigsaw-workspace-" + (''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for i in range(6))) + ".xml"
         # Display iframe:
-        iframe = """<iframe src="%s.html" width="100%%" height="350" style="resize: both; overflow: auto;"></frame>""" % language
+        iframe = """<iframe src="%s.html?workspace=%s" width="100%%" height="350" style="resize: both; overflow: auto;"></frame>""" % (language, workspace)
         self.kernel.Display(HTML(iframe))
 
 def register_magics(kernel):
