f30b2400947aaf228176cf5e6aa2be67bb01edd9
Doug Blank <dblank@cs.brynmawr.edu>
2015-09-24 19:55:59 -0400

    %run magic now takes --language flag to run code in cell magic name

--- metakernel/magics/run_magic.py
+++ metakernel/magics/run_magic.py
@@ -6,23 +6,41 @@ import os
 
 class RunMagic(Magic):
 
-    def line_run(self, filename):
+    @option(
+        '-l', '--language', action='store', default=None,
+        help='use the provided language name as kernel'
+    )
+    def line_run(self, filename, language=None):
         """
-        %run FILENAME - run code in filename by kernel
+        %run [--language LANG] FILENAME - run code in filename by
+           kernel
 
         This magic will take the code in FILENAME and run it. The
         exact details of how the code runs are deterimined by your
         language.
 
-        Example:
+        The --language LANG option will prefix the file contents with
+        "%%LANG". You may also put information in the cell which will
+        appear before the contents of the file.
+
+        Examples:
             %run filename.ss
+            %run -l python filename.py
+
+            %kx calysto_scheme.kernel CalystoScheme
+            %run --language kx filename.ss
+            %run --language "kx default" filename.ss
 
         Note: not all languages may support %run.
         """
         if filename.startswith("~"):
             filename = os.path.expanduser(filename)
         filename = os.path.abspath(filename)
-        self.kernel.do_execute_file(filename)
+        if language is None:
+            self.kernel.do_execute_file(filename)
+        else:
+            code = "".join(open(filename).readlines())
+            self.code = "%%" + language + "\n" + self.code + code
 
 def register_magics(kernel):
     kernel.register_magics(RunMagic)
--- metakernel/magics/tests/test_run_magic.py
+++ metakernel/magics/tests/test_run_magic.py
@@ -1,9 +1,16 @@
 
-from metakernel.tests.utils import get_kernel, EvalKernel, get_log_text
+from metakernel.tests.utils import (get_kernel, EvalKernel, 
+                                    get_log_text, clear_log_text)
 
 def test_run_magic():
     kernel = get_kernel(EvalKernel)
     kernel.do_execute("%%run %s" % __file__.replace(".pyc", ".py"))
+    kernel.do_execute("TEST")
+    text = get_log_text(kernel)
+    assert '42' in text, "Didn't run this file"
+
+    clear_log_text(kernel)
+    kernel.do_execute("%%run --language python %s" % __file__.replace(".pyc", ".py"))
     kernel.do_execute("TEST")
     text = get_log_text(kernel)
     assert '42' in text, "Didn't run this file"
