9bf5aad963dd8f479a792f35e5a6bdfa549afc29
Doug Blank <dblank@cs.brynmawr.edu>
2016-08-11 11:50:24 -0400

    Attempt to get travis tests to pass

--- metakernel/magics/tests/test_ls_magic.py
+++ metakernel/magics/tests/test_ls_magic.py
@@ -1,4 +1,5 @@
 
+import time
 import os
 from metakernel.tests.utils import (get_kernel, get_log_text,
                                     clear_log_text)
@@ -7,11 +8,13 @@ from metakernel.tests.utils import (get_kernel, get_log_text,
 def test_ls_magic():
     kernel = get_kernel()
     kernel.do_execute("%ls /tmp")
+    time.sleep(2)
     text = get_log_text(kernel)
     assert '/tmp/' in text, text[:100]
     clear_log_text(kernel)
 
     kernel.do_execute("%ls /tmp --recursive")
+    time.sleep(2)
     text = get_log_text(kernel)
     assert '/tmp' in text, text[:100]
     clear_log_text(kernel)
