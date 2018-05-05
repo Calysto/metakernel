fa7cec59978af070fa812c2c3eafd6affcceca04
Douglas Blank <doug.blank@gmail.com>
2018-05-05 10:30:36 -0700

    Adjustment for newer jedi

--- metakernel/magics/python_magic.py
+++ metakernel/magics/python_magic.py
@@ -156,7 +156,15 @@ class PythonMagic(Magic):
         position = (info['line_num'], info['column'])
         interpreter = Interpreter(text, [self.env])
 
-        if jedi.__version__ >= LooseVersion('0.10.0'):
+        if jedi.__version__ >= LooseVersion('0.12.0'):
+            lines = split_lines(text)
+            name = get_on_completion_name(
+                interpreter._module_node,
+                lines,
+                position
+            )
+            before = text[:len(text) - len(name)]
+        elif jedi.__version__ >= LooseVersion('0.10.0'):
             lines = split_lines(text)
             name = get_on_completion_name(
                 interpreter._get_module_node(),
