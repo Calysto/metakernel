c9350d39036218f82bff6eb90685d1d3c964cba1
Douglas Blank <doug.blank@gmail.com>
2023-08-29 15:06:49 -0400

    Update tests.yml (#270)
    * Update tests.yml
    
    * Add graphviz where needed
    
    * Add graphviz where needed
    
    * Test for dot binary

--- metakernel/magics/tests/test_dot_magic.py
+++ metakernel/magics/tests/test_dot_magic.py
@@ -1,11 +1,13 @@
-import sys
+import shutil
 
 from metakernel.tests.utils import (get_kernel, get_log_text, 
                                     clear_log_text, EvalKernel)
 
 import pytest
 
-@pytest.mark.skipif(sys.platform != "linux", reason="Requires dot from grahviz")
+NO_DOT = shutil.which("dot") is None
+
+@pytest.mark.skipif(NO_DOT, reason="Requires dot from graphviz")
 def test_dot_magic_cell():
     kernel = get_kernel(EvalKernel)
     kernel.do_execute("""%%dot
@@ -16,7 +18,7 @@ graph A { a->b };
     text = get_log_text(kernel)
     assert "Display Data" in text, text
 
-@pytest.mark.skipif(sys.platform != "linux", reason="Requires dot from grahviz")
+@pytest.mark.skipif(NO_DOT, reason="Requires dot from graphviz")
 def test_dot_magic_line():
     kernel = get_kernel(EvalKernel)
     kernel.do_execute("%dot graph A { a->b };")
