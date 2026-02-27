6875683e856338c041233fb28844a5ed973c1000
Steven Silvester <steven.silvester@gmail.com>
2026-02-27 07:56:21 -0600

    Fix Windows CI: catch ReadTimeout in has_network and set WindowsSelectorEventLoopPolicy

--- conftest.py
+++ conftest.py
@@ -1,3 +1,8 @@
+import asyncio
 import os
+import sys
 
 os.environ["JUPYTER_PLATFORM_DIRS"] = "1"
+
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
